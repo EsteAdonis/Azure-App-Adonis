1e4cd26a5f218ee5ed7497e1824bd79ee81378ce
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-12-18 18:07:45 -0600

    Adding Greeting Message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace azure_app_adonis.Pages;
 
-public class IndexModel(ILogger<IndexModel> logger) : PageModel
+public class IndexModel(ILogger<IndexModel> logger, IConfiguration config) : PageModel
 {
 	private readonly ILogger<IndexModel> _logger = logger;
+	private readonly IConfiguration _config = config;
 
   public void OnGet()
 	{
-		throw new Exception("This is a test exception for Application Insights!");
+		ViewData["Greeting"] = _config["Greeting"];
 	}
 }
